22f5c59cb00eebc3f96b0d27fc4e914fcfd57642
jnluis <jnluis@ua.pt>
2023-02-15 00:39:32 +0000

    Add PDF and printing the file content in a variable

--- lab01/Main.java
+++ lab01/Main.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-import javax.swing.UIDefaults.ProxyLazyValue;
+//import javax.swing.UIDefaults.ProxyLazyValue;
 
 
 public class Main {
@@ -19,22 +19,28 @@ public class Main {
           
           puzzle = new String[linha.length()][linha.length()];
           int count = 0;
-          while (count < linha.length()){
+          while (count < linha.length()-1){ // tem de ter aqui -1 por causa do 1º readLine
             for(int i = 0; i<linha.length(); i++){
-              puzzle[count][i] = linha.substring(i, i);
+              // colocar em cada posição do array puzzle a letra correspondente
+              puzzle[count][i] = linha.substring(i, i+1);
+              
             }
             linha = lerArq.readLine();
-            count++;
+            count++; 
           }
+
+          System.out.println(count);
+          for (int i = 0; i < puzzle.length; i++) {
+            for (int j = 0; j < puzzle[i].length; j++) {
+                System.out.print(puzzle[i][j] + " ");
+            }
+            System.out.println();
+        }
           
           arq.close();
         } catch (IOException e) {
             System.err.printf("Erro na abertura do arquivo: %s.\n",
-              e.getMessage());
+              e.getMessage()); // aqui também podia ser System.out.println("Erro na abertura do arquivo: ");
         }
-
-        System.out.print(puzzle[0][0]);
-    
-        System.out.println();
     }
 }
