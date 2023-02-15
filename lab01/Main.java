4dd37677d8937c169ad5b862900453c14bbec055
dianarrmiranda <dianarrmiranda@ua.pt>
2023-02-15 17:45:48 +0000

    Uso de List<ArrayList<String>> em vez de String[][]

--- lab01/Main.java
+++ lab01/Main.java
@@ -1,46 +1,49 @@
 package lab01;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 //import javax.swing.UIDefaults.ProxyLazyValue;
 
-
 public class Main {
-    public static void main(String[] args) {
-        String file = args[0];
-        String linha = "";
-        String puzzle[][] ;
+  public static void main(String[] args) {
+    String file = args[0];
+    String linha = "";
+    List<ArrayList<String>> puzzle = new ArrayList<ArrayList<String>>();
+    List<String> subPuzzle = new ArrayList<String>();
 
-        try {
-          FileReader arq = new FileReader(file);
-          BufferedReader lerArq = new BufferedReader(arq);
-          linha = lerArq.readLine(); // lê a primeira linha - a variável "linha" recebe o valor "null" quando o processo - de repetição atingir o final do arquivo texto
-          
-          puzzle = new String[linha.length()][linha.length()];
-          int count = 0;
-          while (count < linha.length()-1){ // tem de ter aqui -1 por causa do 1º readLine
-            for(int i = 0; i<linha.length(); i++){
-              // colocar em cada posição do array puzzle a letra correspondente
-              puzzle[count][i] = linha.substring(i, i+1);
-              
-            }
-            linha = lerArq.readLine();
-            count++; 
-          }
+    try {
+      FileReader arq = new FileReader(file);
+      BufferedReader lerArq = new BufferedReader(arq);
+      String linha1 = lerArq.readLine(); // lê a primeira linha do arquivo
 
-          System.out.println(count);
-          for (int i = 0; i < puzzle.length; i++) {
-            for (int j = 0; j < puzzle[i].length; j++) {
-                System.out.print(puzzle[i][j] + " ");
-            }
-            System.out.println();
+      int count = 0;
+
+      while (count < linha1.length()) {
+        subPuzzle = new ArrayList<String>();
+        for (int i = 0; i < linha.length(); i++) {
+          subPuzzle.add(linha.substring(i, i + 1));
         }
-          
-          arq.close();
-        } catch (IOException e) {
-            System.err.printf("Erro na abertura do arquivo: %s.\n",
-              e.getMessage()); // aqui também podia ser System.out.println("Erro na abertura do arquivo: ");
-        }
+        puzzle.add((ArrayList<String>) subPuzzle);
+        linha = lerArq.readLine(); // lê da segunda até a última linha
+        count++;
+      }
+
+      arq.close();
+    } catch (IOException e) {
+      System.err.printf("Erro na abertura do arquivo: %s.\n",
+          e.getMessage()); // aqui também podia ser System.out.println("Erro na abertura do arquivo: ");
     }
+
+    // print do puzzle
+    for (int i = 0; i < puzzle.size(); i++) {
+      for (int j = 0; j < puzzle.get(i).size(); j++) {
+        System.out.print(puzzle.get(i).get(j));
+      }
+      System.out.println();
+    }
+  }
 }
