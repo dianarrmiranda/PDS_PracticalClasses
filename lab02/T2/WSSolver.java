2bc8634bc7b3b8eda7f572fd744ddebe5ad50704
José Luis Oliveira <jlo@ua.pt>
2023-02-24 11:41:35 +0000

    lab02

---
+++ lab02/T2/WSSolver.java
@@ -0,0 +1,14 @@
+package lab01;
+
+import java.io.*;
+
+public class crosswordmain {
+    public static void main(String[] args) {
+      try{
+        File file = new File("source.txt");
+      }catch(Exception e){
+        e.printStackTrace();
+      }
+
+    }
+  }
